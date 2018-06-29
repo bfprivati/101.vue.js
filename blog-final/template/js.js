63480e62b05464249a7e0f922413f37c954fe0e9
Bianca Privati <bfprivati@gmail.com>
2018-06-28 22:31:29 -0300

    adicionado modal

--- blog-final/template/js.js
+++ blog-final/template/js.js
@@ -1,8 +1,7 @@
 const MODAL_WIDTH = 656
 
-Vue.component('ShowLoginModal', {
-    template: "#login",
-    props: ['show'],
+Vue.component('login-modal', {
+    template: "#login-modal",
     data () {
         return {
         modalWidth: MODAL_WIDTH
@@ -13,35 +12,76 @@ Vue.component('ShowLoginModal', {
         ? MODAL_WIDTH / 2
         : MODAL_WIDTH
     },
-    methods: {
-        close: function () {
-            this.$emit('close');
-            this.title = '';
-            this.body = '';
-        },
-        savePost: function () {
-            // Some save logic goes here...
-            
-            this.close();
-        }
-    }
-
 });
+
+Vue.component('post-modal', {
+    template: "#post-modal",
+    data () {
+        return {
+        modalWidth: MODAL_WIDTH
+        }
+    },
+    created () {
+        this.modalWidth = window.innerWidth < MODAL_WIDTH
+        ? MODAL_WIDTH / 2
+        : MODAL_WIDTH
+    },
+});
+
+Vue.component('new-post-modal', {
+    template: "#new-post-modal",
+    data () {
+        return {
+        modalWidth: MODAL_WIDTH
+        }
+    },
+    created () {
+        this.modalWidth = window.innerWidth < MODAL_WIDTH
+        ? MODAL_WIDTH / 2
+        : MODAL_WIDTH
+    },
+});
+
 
 Vue.component('page', {
     template: '#page'
 })
 
+
+
 new Vue({
     el: '#app',
     data: {
+        page: 1,
         title: 'PROJETO: Vuejs',
-        ShowLoginModal: false,
+        login: false,
+        showModal: false,
         menu_list: [
             { title: 'cadastro' },
             { title: 'login' }
         ]
     },
+    components: {
+    },
+    methods: {
+        showLoginModal: function () {
+            this.showModal = true;
+        },
+        showPostModal: function () {
+            this.showModal = true;
+        },
+        showNewPostModal: function () {
+            this.showModal = true;
+        },
+        submitLogin: function(event) {
+            event.preventDefault();
+            document.getElementById('login-form').submit();
+        },
+        submiNewUser: function (event) {
+            event.preventDefault();
+            document.getElementById('login-form').submit();
+        }
+    }
 
 })
 
